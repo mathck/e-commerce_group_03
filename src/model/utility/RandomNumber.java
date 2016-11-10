2c7d8c019c0bd985f0d432488471649d3e489fe8
mathck <mathck@gmail.com>
2016-11-10 19:03:53 +0100

    RandomNumber and gitignore updates

--- src/model/utility/RandomNumber.java
+++ src/model/utility/RandomNumber.java
@@ -1,18 +1,17 @@
 package model.utility;
 
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class RandomNumber {
 
-    // TODO Mateusz
+    private static final int standardDeviation = 1;
 
-    public static int numberBetween(int min, int max) {
-        return 0;
+    public static int nextInt(int min, int max) {
+        return ThreadLocalRandom.current().nextInt(min, max + 1);
     }
 
-    public static int gaussian(int mean) {
-        // returne am öftesten mean
-        // am 2t öftesten mean-1 und mean+1
-        // am 3t öftesten mean-2 und mean+2
-        // ...
-        return mean;
+    public static int nextGaussian(int mean) {
+        return (int) Math.round(new Random().nextGaussian() * standardDeviation + mean);
     }
 }
