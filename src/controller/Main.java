010ef71a3268c0acba07e08f6d7ce2abb5dbbb3c
mathck <mathck@gmail.com>
2016-11-10 20:33:41 +0100

    refac

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -5,7 +5,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import model.implementations.Grid;
 import model.interfaces.INode;
